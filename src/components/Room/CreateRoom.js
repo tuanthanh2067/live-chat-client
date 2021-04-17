31153f2b43f57a8fe6dbfe481b4c9cee9a51c3f3
tuanthanh2067 <tuanthanh2067@gmail.com>
2021-04-17 15:54:53 -0400

    adding form validation for create room

--- src/components/Room/CreateRoom.js
+++ src/components/Room/CreateRoom.js
@@ -1,45 +1,83 @@
 import styled from "styled-components";
 
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
 const CreateRoom = () => {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      description: "",
+      visibility: "public",
+      max: "",
+    },
+    validationSchema: Yup.object({
+      name: Yup.string().required("Room name is required"),
+      description: Yup.string().required("Description is required"),
+      visibility: Yup.string().required("Visibility is required"),
+      max: Yup.number()
+        .typeError("Value must be a number")
+        .required("This field is required")
+        .positive("Must be more than zero"),
+    }),
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
   return (
     <StyledCreateRoom>
-      <StyledForm onSubmit>
+      <StyledForm onSubmit={formik.handleSubmit}>
         <StyledFormGroup>
-          <label for="name">Room name</label>
+          <label>Room name</label>
           <input
             type="text"
             name="name"
             placeholder="League of legends"
+            value={formik.values.name}
+            onChange={formik.handleChange}
           ></input>
+          <StyledError>{formik.errors.name}</StyledError>
         </StyledFormGroup>
 
         <StyledFormGroup>
-          <label for="description">Description</label>
+          <label>Description</label>
           <input
             type="text"
             name="description"
             placeholder="Let's go play some games together"
+            value={formik.values.description}
+            onChange={formik.handleChange}
           ></input>
+          <StyledError>{formik.errors.description}</StyledError>
         </StyledFormGroup>
 
         <StyledFormGroup>
-          <label for="visibility">Room visibility</label>
-          <select name="visibility">
-            <option value="private">Private</option>
+          <label>Room visibility</label>
+          <select
+            name="visibility"
+            value={formik.values.visibility}
+            onChange={formik.handleChange}
+          >
             <option value="public">Public</option>
+            <option value="private">Private</option>
           </select>
+          <StyledError>{formik.errors.visibility}</StyledError>
         </StyledFormGroup>
 
         <StyledFormGroup>
-          <label for="max">Max members</label>
+          <label>Max members</label>
           <input
-            type="number"
+            type="text"
             name="max"
             placeholder="Default maximum members: 300"
+            value={formik.values.max}
+            onChange={formik.handleChange}
           ></input>
+          <StyledError>{formik.errors.max}</StyledError>
         </StyledFormGroup>
 
-        <button>Create now</button>
+        <input type="submit" value="Create now"></input>
       </StyledForm>
     </StyledCreateRoom>
   );
@@ -51,12 +89,12 @@ const StyledCreateRoom = styled.div`
 `;
 
 const StyledForm = styled.form`
-  max-width: 500px;
+  max-width: 600px;
   min-width: 300px;
-  width: 50%;
+  width: 60%;
   height: 100%;
 
-  button {
+  input[type="submit"] {
     padding: 12px 32px;
     background: transparent;
     border: 1px solid grey;
@@ -82,15 +120,23 @@ const StyledFormGroup = styled.div`
     font-size: 1.5em;
   }
 
-  input,
+  input[type="text"],
   select {
     background: transparent;
     padding: 0.85em 1.25em;
     border: 1px solid grey;
     border-radius: 24px;
-    outline: none;
+    outline: transparent;
     color: grey;
   }
+`;
+
+const StyledError = styled.p`
+  color: red;
+  margin-top: 0.25em;
+  padding-left: 0.75em;
+
+  font-size: 0.9em;
 `;
 
 export default CreateRoom;
